6a2b0186eae18f54688b0db08894ed4263a6ae31
SnapDragon <maliktahir0702@gmail.com>
2019-10-02 20:37:49 +0200

    Create power_tools.py

---
+++ userbot/plugins/power_tools.py
@@ -0,0 +1,20 @@
+import asyncio
+import os
+import sys
+
+
+@command(pattern="^.restart", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Restarting. `.ping` me or `.helpme` in a few minutes to check if I am online")
+    await bot.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+@command(pattern="^.shutdown", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Turning off ...Manually turn me on later")
+    await bot.disconnect()
