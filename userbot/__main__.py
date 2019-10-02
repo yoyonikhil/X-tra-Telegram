33ddc1ac463c2b2481bdce25ec75ff0eb4fd7ec2
SnapDragon <maliktahir0702@gmail.com>
2019-10-02 21:46:26 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -18,17 +18,19 @@ async def add_bot(bot_token):
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    print("Initiating Inline Bot")
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+    print("Starting Userbot")
     bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-    print("Initialisation finished with no errors")
+    print("Startup Completed")
 
 import glob
 path = 'userbot/plugins/*.py'
