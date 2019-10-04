c59b316ca3edd19e6d117f780bef1c736f383896
SnapDragon <maliktahir0702@gmail.com>
2019-10-04 21:44:32 +0200

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -5,9 +5,9 @@ async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         string = ""
         for i in CMD_LIST:
-            string += i + "\n"
+            string += "ℹ️ " + i + "\n"
             for iter_list in CMD_LIST[i]:
-                string += "    ℹ️ `" + str(iter_list) + "`"
+                string += "    `" + str(iter_list) + "`"
                 string += "\n"
             string += "\n"
         if len(string) > 4095:
