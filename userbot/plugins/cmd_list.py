d44cf096c7fb688aa93f07d67247fd28957f1617
SnapDragon <maliktahir0702@gmail.com>
2019-10-04 15:46:24 +0200

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -3,7 +3,7 @@ from userbot import CMD_LIST
 @command(pattern="^.get_cmd")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        string = ""
+        string = "Ignore \ and ^:\n\n"
         for i in CMD_LIST:
             string += "ℹ️ `" + str(i)
             string += "`\n"
