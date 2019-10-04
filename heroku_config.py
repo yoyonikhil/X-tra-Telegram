4cb815757968642afa2d72d8247ea4cd6b0357b0
SnapDragon <maliktahir0702@gmail.com>
2019-10-04 12:39:31 +0200

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -8,6 +8,8 @@ class Var(object):
     DB_URI = os.environ.get("DATABASE_URL", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
     LOGGER = True
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
     SUDO_USERS = os.environ.get("SUDO_USERS", "719877937")
     LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
