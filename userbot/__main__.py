a5a38f7ff266b6dfe4b043b146794f51df60141d
SnapDragon <maliktahir0702@gmail.com>
2019-10-04 23:27:39 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -31,9 +31,11 @@ else:
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished with no errors")
-    print("Starting Userbot")
-    bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-    print("Startup Completed")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
     
 
 import glob
