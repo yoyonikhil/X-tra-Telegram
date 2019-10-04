01ebc0c77ebfe5b86da36955c88d6204ede726a2
SnapDragon <maliktahir0702@gmail.com>
2019-10-04 14:05:41 +0200

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -7,7 +7,7 @@ import sys
 async def _(event):
     if event.fwd_from:
         return
-    asyncio.get_event_loop().create_task(restart())
+    asyncio.get_event_loop().create_task(restart(event))
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
@@ -15,13 +15,13 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    asyncio.get_event_loop().create_task(shutdown())
+    asyncio.get_event_loop().create_task(shutdown(event))
 
 
-async def restart():
+async def restart(event):
     await bot.disconnect()
     await event.edit("Restarting. `.ping` me or `.helpme` in a few minutes to check if I am online")
 
-async def shutdown():
+async def shutdown(event):
     await bot.disconnect()
     await event.edit("Turning off ...Manually turn me on later")
