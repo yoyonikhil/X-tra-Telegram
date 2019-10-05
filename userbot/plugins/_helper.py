03f683dead0b3ada0a92e4a0d7aa78f5265ce566
SnapDragon <maliktahir0702@gmail.com>
2019-10-05 11:31:52 +0200

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,6 +1,6 @@
 from userbot import CMD_LIST
 
-@command(pattern="^.help")
+@command(pattern="^.help ?(.*)")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
