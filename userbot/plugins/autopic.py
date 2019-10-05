f5d0d6d0ede5b49e2656f041f9a1e85dadd36185
SnapDragon <maliktahir0702@gmail.com>
2019-10-05 16:43:06 +0200

    Update autopic.py

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -27,13 +27,13 @@ async def autopic(event):
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
         drawn_text.text((50, 250), current_time, font=fnt, fill=(255, 255, 255))
         img.save(photo)
-        file = await bot.upload_file(photo_complete)  # pylint:disable=E0602
+        file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
             await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
                 file
             ))
             os.remove(photo)
             counter -= 5
-            await asyncio.sleep(60)
+            await asyncio.sleep(65)
         except:
             return
